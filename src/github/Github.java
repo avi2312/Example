e9acc0435b0b0c474e88cfe0fe63c7327ba56733
avina <avina@LAPTOP-N8LAJE5L>
2022-02-07 09:11:14 +0530

    First git program

---
+++ src/github/Github.java
@@ -0,0 +1,15 @@
+package github;
+
+public class Github {
+
+	public Github() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am in git repositary");
+
+	}
+
+}
