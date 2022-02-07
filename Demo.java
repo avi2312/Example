5b33f065e75ddc6e54504be1730cfcfd46b11652
avi2312 <76042533+avi2312@users.noreply.github.com>
2022-02-07 09:58:51 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package github;
+
+public class Demo {
+
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("U have successfully enter in the git hub repositary");
+	}
+
+}
